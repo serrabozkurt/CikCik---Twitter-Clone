fbce52a5b37e3ad774169a3808f3f4a7865b4887
serrabozkurt <56247446+serrabozkurt@users.noreply.github.com>
2023-01-21 12:25:39 +0300

    Introduction to Python

---
+++ Step 01 - Intorduction to Python, Variables/hello.py
@@ -0,0 +1,21 @@
+print("Merhaba dünya!")
+
+"""
+Hayat kısa
+Kuşlar uçuyor
+"""
+
+print()
+print("Merhaba Python <3")
+
+print("Merhaba Onur!", "Hoş geldin")
+print("Boy:", 2.0000000001, "m")
+
+isim = "Gökçe"  # print("Onur")
+boy = 190  # cm
+kilo = 75.5  # kg
+# doğru/yanlış bilgisi
+python_biliyor = True
+spor_yapiyor = False
+
+print("Adı:", isim)
