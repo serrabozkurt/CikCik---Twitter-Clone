a8a623d53f270a34cfc795d8657a4e5121668f33
serrabozkurt <56247446+serrabozkurt@users.noreply.github.com>
2023-01-21 12:26:11 +0300

    operators and conditions

---
+++ Step 02 - Operators/conditions.py
@@ -0,0 +1,26 @@
+x = 16
+
+print("Bu mesaj her zaman görünür!")
+
+if x < 10:
+    print(x)
+    print("x, 10'dan küçük")
+elif x > 10:
+    print("x, 10'dan büyük")
+elif x == 10:
+    print("x, 10'a eşit")
+else:
+    print("Matematik bozuk")
+
+
+if x < 10:
+    print(x)
+    print("x, 10'dan küçük")
+if x > 10:
+    print("x, 10'dan büyük")
+else:
+    print("x, 10'a eşit")
+
+
+print("Program sonlandı")
+print("Gerçekten sonlandı")
---
+++ Step 02 - Operators/operations.py
@@ -0,0 +1,100 @@
+toplam = 12 + 34
+
+print(toplam)
+
+print(5 + 7)
+
+print(2.6 + 3.9)
+
+print(2 + 3.4)
+
+print(9 - 5)
+
+print(4 - 7)
+
+print(-3 - 20)
+
+print(4 - 5.3)
+
+print(3 * 6)
+
+print(0.1 * 3)
+
+print(10 / 2)
+
+print(2.5 + 2.5)
+
+print(3 / 4)
+
+print(13 // 5)
+
+print(13.4 // 5)
+
+print(13 % 5)
+
+
+
+print("merhaba" + "dünya")
+
+# metin sadece başka bir metin ile toplanır
+# print("merhaba" + 8)
+
+# metinlerde çıkarma işlemi olmaz!
+# print("merhaba" - "mer")
+
+print("merhaba" * 5)
+
+
+
+
+
+x = 7
+
+# önce işlem, sonra atama
+x = x + 5
+print(x)
+
+# üstteki işlemin kısa hâli
+x += 5
+print(x)
+
+x -= 6
+print(x)
+
+x *= 2
+print(x)
+
+x /= 10
+print(x)
+
+y = 14
+y //= 4
+print(y)
+
+z = 14
+z %= 4
+print(z)
+
+a = "merhaba "
+a += "dünya"
+print(a)
+
+b = "Hey"
+b *= 10
+print(b)
+
+c = "merhaba"
+c += "python" * 5
+print(c)
+
+d = "merhaba"
+d *= 5 + 7
+# d = d * (5 + 7)
+print(d)
+
+aa = 3 * 4 + 5 / 7
+print(a)
+ab = 3 * (4 + 5 / 7)
+print(ab)
+ac = 3 * (4 + (5 / 7))
+print(ac)
